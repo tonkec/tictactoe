3270bde849b5092c447ce7bf5ce546cc1ddb9f9f
tonkec <antonija1023@gmail.com>
2024-10-10 12:29:36 +0200

    feat(project-setup) init project with Vite

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#CC59D2',
+        'secondary': '#FFF3F0',
+      },
+    },
+  },
+  plugins: [],
+}
+
