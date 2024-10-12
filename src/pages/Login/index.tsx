aed77e7edb3cf014ea0fce4e59746d1e2c2b8b6e
tonkec <antonija1023@gmail.com>
2024-10-12 09:11:46 +0200

    feat(abs-imports) add absolute imports

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,21 +1,37 @@
-import { AuthContainer } from "../../ui/auth"
-import { Container } from "../../ui/container"
-import { AuthForm } from "../../components/AuthForm";
-import { AuthLink } from "../../components/AuthLink";
-import { useLoginUser } from "./hooks";
-import { IUserProps } from "./Login.interface";
+import { AuthContainer } from '@/ui/auth';
+import { Container } from '@/ui/container';
+import { AuthForm } from '@/components/AuthForm';
+import { AuthLink } from '@/components/AuthLink';
+import { useLoginUser } from './hooks';
+import { IUserProps } from './Login.interface';
 
 export const Login = () => {
-    const { loginUser, isSignupError, isCreating, isSuccess } = useLoginUser();
-    const message = isSignupError ? "Something went wrong" : isSuccess ? "You are logged in" : ""
+  const { loginUser, isSignupError, isCreating, isSuccess } = useLoginUser();
+  const message = isSignupError
+    ? 'Something went wrong'
+    : isSuccess
+      ? 'You are logged in'
+      : '';
 
-    const onSubmit = (data: IUserProps) => {
-        loginUser(data)
-    }
+  const onSubmit = (data: IUserProps) => {
+    loginUser(data);
+  };
 
-    return <Container>
-        <AuthContainer>
-            <AuthForm isButtonDisabled={isCreating} message={message} signupButtonText="Login" onSubmit={onSubmit} headline="Welcome back!" subtitle="Login and start playing." linkComponent={() => <AuthLink title="New user? Signup" href="/signup" />} />
-        </AuthContainer>
+  return (
+    <Container>
+      <AuthContainer>
+        <AuthForm
+          isButtonDisabled={isCreating}
+          message={message}
+          signupButtonText="Login"
+          onSubmit={onSubmit}
+          headline="Welcome back!"
+          subtitle="Login and start playing."
+          linkComponent={() => (
+            <AuthLink title="New user? Signup" href="/signup" />
+          )}
+        />
+      </AuthContainer>
     </Container>
-}
+  );
+};
