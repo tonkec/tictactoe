6df1d2b9affbb2c611b3d6eb3bbd97ed3bf47167
tonkec <antonija1023@gmail.com>
2024-10-12 07:43:51 +0200

    feat(logout) remove token

--- src/components/Nav/hooks/index.ts
+++ src/components/Nav/hooks/index.ts
@@ -1,14 +1,17 @@
 import { useMutation } from '@tanstack/react-query';
 import { logout} from '../../../api/auth/logout';
 import { useNavigate } from 'react-router-dom';
+import { useLocalStorage } from '@uidotdev/usehooks';
 
 function useLogoutUser() {
     const navigate = useNavigate();
+    const [, saveAuthToken] = useLocalStorage("token", null);
 
     const { mutate: logoutUser, isPending: isCreating, isError: isLogoutError, isSuccess } = useMutation({
       mutationFn: () =>
         logout(),
       onSuccess: () => {
+        saveAuthToken(null);
         navigate('/login');
       },
       onError: (err: Error) => console.log('error while logging out:', err.message), 
